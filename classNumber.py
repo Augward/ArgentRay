12cf18269deb2643066bbf13fabed949ce7b05f5
Augie <ward31513@gmail.com>
2025-03-21 16:41:05 -0500

    Update classNumber.py
    reorganized and made 11-16 bases work

--- classNumber.py
+++ classNumber.py
@@ -5,7 +5,7 @@
 File name: classNumber
 Author: Augustin Ward
 Created: 2025-16-03
-Version: 1.2
+Version: 1.3
 Description: Class that establishes a type of number
 
 License: NA
@@ -34,34 +34,46 @@ class Number:
     # N = Natural (1,2,3), W = Whole (0,1,2), Z = Integer (-1,0,1), R = Real (-pi, 0, pi)
     # Q = Rational (0.5, 1/2, 1/9), I = Irrational (unable), C = Complex (ai + b)
     # Base saved = 2, 4, 8, 10, 16; able to compute any base
+    
     def __init__(self, number, element = None):
         
-        if type(number) != int and type(number) != float:
-            raise TypeError("number must be: int, float, or Number")
+        self.ogNumber = number
         
-        isNegative = -1 if number < 0 else 1
-        number = abs(number)
+        if not isinstance(number, (int, float, str)):
+            raise TypeError("number must be: int, float, or str")        
         
-        if type(element) == int:
-            if element >= 2 and element <= 16:
-                self.number = int(max(0, math.floor(number)))
-                self.numType = "W"
-                
+        
+        if isinstance(element, int):
+            if 2 <= element <= 16:
                 self.ogBase = element
                 self.base = element
                 
-                self.number = int(self.convertBase(10)) * isNegative
+                if not isinstance(number, str):
+                    number = int(math.floor(number))
+                    
+                    isNegative = -1 if number < 0 else 1
+                    self.number = abs(number)
+                    
+                    self.number = int(self.convertBase(10)) * isNegative
+                    
+                else:
+                    isNegative = -1 if (number[0] == "-") else 1
+                    self.number = number.lstrip("-")
+                    
+                    self.number = int(self.number, self.base) * isNegative
+                
+                self.numType = "W"
                 self.base = 10
                 
             else:
                 raise ValueError("base must be: >= 2, and <= 16")
-        
-        
-        elif type(element) == str:
+            
+            
+        elif isinstance(element, str):
+            self.ogBase = 10
+            self.base = 10
+            
             if len(element) == 1:
-                self.ogBase = None
-                self.base = 10
-                
                 if element == "N":
                     self.number = int(max(1, math.floor(number)))
                     self.numType = "N"
@@ -71,23 +83,23 @@ class Number:
                     self.numType = "W"
                     
                 elif element == "Z":
-                    self.number = int(math.floor(number)) * isNegative
+                    self.number = int(math.floor(number))
                     self.numType = "Z"
                     
                 elif element == "Q":
-                    self.number = round(float(number),12) * isNegative
+                    self.number = round(float(number),12)
                     self.numType = "Q"
                     
                 elif element == "I":
-                    self.number = float(number) * isNegative
+                    self.number = float(number)
                     self.numType = "I"
                     
                 elif element == "C":
-                    self.number = complex(number) * isNegative
+                    self.number = complex(number)
                     self.numType = "C"
                     
                 elif element == "R":
-                    self.number = number * isNegative
+                    self.number = number
                     self.numType = "R"
                 
                 else:
@@ -97,9 +109,10 @@ class Number:
                 raise ValueError("numType must be only one character long")
         
         
-        elif type(element) == None:
-            self.number = number * isNegative
+        elif element == None:
+            self.number = number
             self.numType = "R"
+            self.ogBase = 10
             self.base = 10
             
             
@@ -116,7 +129,7 @@ class Number:
 
 
     def __add__(self, other):
-        self.number + other.number
+        self.number + other.number # Not done
         
         
     def __radd__(self, other):
@@ -136,11 +149,10 @@ class Number:
             decimal = int(self.number)
         
         if newBase == 10:
-            return decimal
+            return int(decimal)
             
         isNegative2 = -1 if decimal < 0 else 1
         decimal = abs(decimal)
-            
             
         digits = "0123456789ABCDEF"
         result = ""
@@ -154,19 +166,29 @@ class Number:
 
 
 '''
-x = Number(-16, 16)
+x = Number("-1E", 16)
 print(x)
-x3 = x.number
-x4 = x.convertBase(14)
-x5 = x.numType
-x6 = x.base
+x2 = x.number
+x3 = x.ogNumber
+x4 = x.numType
+x5 = x.base
+x6 = x.ogBase
 
-y = Number(-44.4, "R")
+y = Number(-44.4, "Z")
 print(y)
-y2 = y.convertBase(8)
-y3 = y.numType
-y4 = y.number
+y2 = y.number
+y3 = y.ogNumber
+y4 = y.numType
+y5 = y.base
+y6 = y.ogBase
 
-z = Number(y2, 8)
-print(z)
+'''
+
+'''
+englishLength = {'in':1, 'ft':12, 'yd':36, 'mi':6360}
+metricLength = {'mm':0.001, 'cm': 0.01, 'dm': 0.1, 'm':1, 'dam':10, 'hm':100, 'km':100}
+lengthConversionME = x * 39.37/12 # meter to foot
+lengthConversionEM = x * 12/39.37 # foot to meter
+
+units = {'in':1, 'ft':12, 'yd':36, 'mi':6360, 'mm':0.001, 'cm': 0.01, 'dm': 0.1, 'm':1, 'dam':10, 'hm':100, 'km':100}
 '''
